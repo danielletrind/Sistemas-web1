099d987e6a390d0255271ef91ee889d659563b78
Danielle Trindade <100931893+danielletrind@users.noreply.github.com>
2023-05-02 09:58:52 -0300

    Add files via upload

---
+++ Atividade_18_04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Atividade_18_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Banco por : Danielle Trindade");
+        Banco b = new Banco();
+
+        b.Depositar(1000);
+        b.Sacar(1600);
+
+        b.AjustarLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+
+        Console.WriteLine("O seu saldo total é de " + valortotal);
+ 
+
+        b.imprimi(b.emprestimo(12,1000));
+    }
+}

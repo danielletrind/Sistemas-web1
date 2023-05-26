c0037f469fa46bde636b9fd0e71ac826a5435c0f
Danielle Trindade <100931893+danielletrind@users.noreply.github.com>
2023-05-26 17:55:06 -0300

    Add files via upload

---
+++ exercicios_sw/exercicio_01.cs
@@ -0,0 +1,16 @@
+﻿namespace exercicio_01;
+class Program
+{
+ static void Main(string[] args)
+    {
+       Animal a = new Animal();
+        Homem h = new Homem();
+          Cao c = new Cao();
+         Gato g = new Gato();
+
+
+        Console.WriteLine("Homem é: " + h.Fala());
+        Console.WriteLine("Cão é: " + c.Fala());
+        Console.WriteLine("Gato é: " + g.Fala());
+    }
+}

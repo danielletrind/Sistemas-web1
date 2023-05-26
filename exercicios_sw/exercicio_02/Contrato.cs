8e406abaf09de770a2df3b6b524227460cddde37
Danielle Trindade <100931893+danielletrind@users.noreply.github.com>
2023-05-26 18:32:31 -0300

    Add files via upload

---
+++ exercicios_sw/exercicio_02/Contrato.cs
@@ -0,0 +1,18 @@
+﻿namespace exercicio_02;
+class Contrato
+
+{
+   public string? Nome {get;set;}
+
+   public string? Email {get;set;}
+
+   public string? Telefone {get;set;}
+
+   public virtual string MostraDados(){
+
+      string Tudo = this.Nome + "-" + this.Email + "-" + this.Telefone;
+      return Tudo;
+   }
+
+
+}
---
+++ exercicios_sw/exercicio_02/Program.cs
@@ -0,0 +1,24 @@
+﻿﻿namespace exercicio_02;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contrato c = new Contrato();
+        ContratoPessoaFisica cf = new ContratoPessoaFisica();
+        ContratoPessoaJuri cj = new ContratoPessoaJuri();
+
+        cf.Telefone = "48228922";
+        cf.Email = "danitrindade@gmail.com";
+        cf.Nome = "Danielle";
+        cf.Idade = 16;
+        cf.Cpf = 24356870923;
+        cj.Nome = "Etec";
+        cj.Email = "etec3546@gmail.com";
+        cj.Telefone = "12345678";
+        cj.Cnpj = 35473859407621;
+        cj.Inscricao = 243657890345;
+
+        Console.WriteLine(cf.MostraDados());
+        Console.WriteLine(cj.MostraDados());
+    }
+} 
